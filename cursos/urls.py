8b880d6568cc01e572e7f35781bcc35fd13789f9
davidcatalan9 <dcatalan@gmail.com>
2023-08-14 13:01:42 -0400

    funcioando ok lunes AM

--- cursos/urls.py
+++ cursos/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 app_name = 'cursos'
 urlpatterns = [
-    path( 'listaCursos', views.listaCursos, name='listaCursos'),
+    path( '', views.listaCursos, name='listaCursos'),
     path('detalleCurso/<int:curso_id>',views.detalleCurso,name='detalleCurso')
 ]
 
